251bb1b4a907406f36d5a45cbf5812f7ecec01b7
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-07-14 18:54:50 +0000

    Fixed :hover delete button display

--- src/js/component/SingleTaskComponent.jsx
+++ src/js/component/SingleTaskComponent.jsx
@@ -3,8 +3,13 @@ import React, { useState } from 'react'
 export const SingleTaskComponent = ({ task, onDelete }) => {
 
     return (
-        <li className="p-3"
-            onClick={() => onDelete(task)}>{task} <i className="fa-2x ms-5bi bi-x-circle-fill bg-lighttext-align-start mt-3 text-danger rounded p-2"></i>
+        <li className="p-3 d-flex justify-content-between" onClick={() => onDelete(task)}>
+            <div className="d-flex align-items-center">
+                {task}
+            </div>
+            <div>
+                <span className="delete-btn ">  <i className="fa-2x ms-5bi bi-x-circle-fill bg-lighttext-align-start mt-3 text-danger rounded p-2"></i></span>
+            </div>
         </li>
     )
 }
